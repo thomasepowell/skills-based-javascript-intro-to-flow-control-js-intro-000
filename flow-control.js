0e2dcbb65dc6842f163e4f24965e3ed1d49fe3dc
glorious-tier-7619 <thomasepowell@icloud.com>
2019-04-15 19:24:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,25 @@ return (age >= 13 && age <= 19 ) ? "You are a teenager" : "You are not a teenage
 
 function switchAge(age) {
   switch(age) {
-    case (age >= 13 && age <= 19):
+    case 13:
+    return "You are a teenager";
+    break;
+    case 14:
+    return "You are a teenager";
+    break;
+    case 15:
+    return "You are a teenager";
+    break;
+    case 16:
+    return "You are a teenager";
+    break;
+    case 17:
+    return "You are a teenager";
+    break;
+    case 18:
+    return "You are a teenager";
+    break;
+    case 19:
     return "You are a teenager";
     break;
     default:
